70683fdd02cc86aec29471400ec58df7ef45f4d8
Andrew Lor <andrewlor332@gmail.com>
2019-02-09 09:32:15 -0800

    basic input impl

---
+++ src/components/Input/Input.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import * as R from 'ramda'
+
+import './Input.scss'
+
+export default class Input extends React.PureComponent {
+
+  computeClassNames = () =>
+    R.join(' ', ['input'])
+  
+  render = () =>
+    <input
+      className={this.computeClassNames()}
+      type={this.props.type}
+      onChange={this.props.onChange}
+      onBlur={this.props.onBlur}
+      placeholder={this.props.placeholder}
+    />
+}
+
+Input.propTypes = {
+  type: PropTypes.oneOf(['text', 'textarea', 'number']),
+  onChange: PropTypes.function,
+  onBlur: PropTypes.function,
+  placeholder: PropTypes.String,
+}
--- src/demo.js
+++ src/demo.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 import Button from './components/Button'
+import Input from './components/Input'
 
 class Demo extends React.Component {
   render = () =>
     <div>
+      <Input placeholder='Email' />
       <Button text='Normal' onClick={() => alert('Click')} />
       <Button text='Cancel' type='cancel' />
       <Button text='Save' type='save' />
